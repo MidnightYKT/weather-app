9c1ab617e3a3234cd7bf9b837d99d27b86904943
MidnightYKT <nomiama_ykt@mail.ru>
2024-04-01 16:45:26 -0700

    new rep

---
+++ app/component/Input.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { AiOutlineSearch } from "react-icons/ai";
+
+const Input = () => {
+  return (
+    <form className="flex items-center md:w-2/4 w-full order-2 md:order-1">
+      <input
+        type="text"
+        placeholder="Search City"
+        className="w-full bg-transparent border-b-2 placeholder-white text-white outline-none"
+      />
+      <div className="ml-[-25px] text-white cursor-pointer">
+        <AiOutlineSearch />
+      </div>
+    </form>
+  );
+};
+
+export default Input;
